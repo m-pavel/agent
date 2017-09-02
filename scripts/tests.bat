48efa7b61d0f532c39ca9af2893a0aebe7f2aa66
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 20:19:44 +1000

    Mount onto c:, like it's 1991

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -2,7 +2,7 @@
 
 echo '+++ Running tests'
 docker run --rm ^
-  -v %cd%:/go/src/github.com/buildkite/agent ^
-  -w /go/src/github.com/buildkite/agent ^
-  golang:1.9-windowsservercore ^
+  -v %cd%:c:\gopath\src\github.com\buildkite\agent ^
+  -w c:\go\src\github.com\buildkite\agent ^
+  golang:1.9-nanoserver ^
   go test ./...
