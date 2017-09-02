7cebc9b1c61de533cafdeda4192757cd7d7aa802
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 21:11:27 +1000

    Don't use a tty

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -1,5 +1,5 @@
 @echo off
 
 docker build -f .\Dockerfile-windows --tag buildkiteagent%BUILDKITE_BUILD_NUMBER% .
-docker run --rm -it buildkiteagent%BUILDKITE_BUILD_NUMBER% go test ./...
-docker rm buildkiteagent%BUILDKITE_BUILD_NUMBER%
+docker run --rm buildkiteagent%BUILDKITE_BUILD_NUMBER% go test ./...
+docker rmi buildkiteagent%BUILDKITE_BUILD_NUMBER%
