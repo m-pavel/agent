feb270bd6529e664703b007ad67c612dd157acab
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 19:56:13 +1000

    Apparently caret is used for multi-line

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -1,8 +1,8 @@
 @echo off
 
 echo '+++ Running tests'
-docker run --rm \
-  -v %cd%:/go/src/github.com/buildkite/agent \
-  -W /go/src/github.com/buildkite/agent \
-  golang:1.9-windowsservercore \
+docker run --rm ^
+  -v %cd%:/go/src/github.com/buildkite/agent ^
+  -W /go/src/github.com/buildkite/agent ^
+  golang:1.9-windowsservercore ^
   go test ./...
