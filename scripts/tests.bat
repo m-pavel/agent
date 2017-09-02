7098e8811227e47fead18fbda40b9287f29d9017
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 20:00:03 +1000

    Set working dir

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -3,6 +3,6 @@
 echo '+++ Running tests'
 docker run --rm ^
   -v %cd%:/go/src/github.com/buildkite/agent ^
-  -W /go/src/github.com/buildkite/agent ^
+  -w /go/src/github.com/buildkite/agent ^
   golang:1.9-windowsservercore ^
   go test ./...
