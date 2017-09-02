dcdc025234745f8fc5a7ff5a564f1ab37d3594d6
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 19:42:04 +1000

    Run go test in a windows golang container

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -1,3 +1,8 @@
 @echo off
+
 echo '+++ Running tests'
-go test ./...
+docker run --rm \
+  -v %cd%:/go/src/github.com/buildkite/agent \
+  -W /go/src/github.com/buildkite/agent \
+  golang:1.9-windowsservercore \
+  go test ./...
