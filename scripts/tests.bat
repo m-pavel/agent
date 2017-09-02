c6e6cca145323d4b3d8f4ff64febf5d6d2e53fff
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 21:15:08 +1000

    Exit on failure in batch

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -1,5 +1,11 @@
 @echo off
 
-docker build -f .\Dockerfile-windows --tag buildkiteagent%BUILDKITE_BUILD_NUMBER% .
-docker run --rm buildkiteagent%BUILDKITE_BUILD_NUMBER% go test ./...
+docker build -f .\Dockerfile-windows --tag buildkiteagent%BUILDKITE_BUILD_NUMBER% . || goto :error
+docker run --rm buildkiteagent%BUILDKITE_BUILD_NUMBER% go test ./... || goto :error
+goto :EOF
+
+:error
+set previous_errorlevel=%errorlevel%
 docker rmi buildkiteagent%BUILDKITE_BUILD_NUMBER%
+echo Failed with error #%previous_errorlevel%.
+exit /b %previous_errorlevel%
