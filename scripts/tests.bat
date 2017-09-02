7938b1eacedcb4b1f9f588905529aa358dade211
Lachlan Donald <lachlan@buildkite.com>
2017-09-02 21:02:38 +1000

    Add a windows Dockerfile

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -1,8 +1,7 @@
 @echo off
 
-echo '+++ Running tests'
-docker run --rm ^
-  -v %cd%:c:\gopath\src\github.com\buildkite\agent ^
-  -w c:\go\src\github.com\buildkite\agent ^
-  golang:1.9-nanoserver ^
-  go test ./...
+set image_name=buildkite-agent-%BUILDKITE-BUILD_NUMBER%
+
+docker build -f .\Dockerfile-windows --tag %image_name% .
+docker run --rm -it %image_name% go test ./...
+docker rm %image_name%
