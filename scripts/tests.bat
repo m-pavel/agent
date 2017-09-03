0d23a1568c14b539bded7f57e6958302f2ecf52b
Lachlan Donald <lachlan@buildkite.com>
2017-09-03 19:56:14 +1000

    Install openssh into windows container

--- scripts/tests.bat
+++ scripts/tests.bat
@@ -1,14 +1,17 @@
 @echo off
 
+rem TODO generate random suffix
+echo image_suffix=xxxxx
+
 echo --- Building docker image
-docker build -f .\Dockerfile-windows --tag buildkiteagent%BUILDKITE_BUILD_NUMBER% . || goto :error
+docker build -f .\Dockerfile-windows --tag buildkiteagent%image_suffix% . || goto :error
 
 echo +++ Running tests
-docker run --rm buildkiteagent%BUILDKITE_BUILD_NUMBER% go test -v ./... || goto :error
+docker run --rm buildkiteagent%image_suffix% go test -v ./... || goto :error
 goto :EOF
 
 :error
 set previous_errorlevel=%errorlevel%
-docker rmi buildkiteagent%BUILDKITE_BUILD_NUMBER%
+docker rmi buildkiteagent%image_suffix%
 echo Failed with error #%previous_errorlevel%.
 exit /b %previous_errorlevel%
